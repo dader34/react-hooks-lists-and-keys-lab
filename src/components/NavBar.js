0a53899f01e108574db4e9926a04112b68c11e1d
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-02 16:40:08 -0700

    Passed all tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+
+function RenderLink({links}) {
+  const linksArr = links.map((e,idx) =>{
+    return(
+      <a key = {idx} href={"#"+e}>{e}</a>
+    )
+  })
+  return (
+    linksArr
+  )
+}
+
+
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav><RenderLink links={links}/></nav>
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function RenderSpan({technologies}){
+  console.log(technologies)
+  const newList = technologies.map((e,idx) =>{
+    return(
+      <span key={idx}>
+        {e}
+      </span>
+    )
+  })
+  return newList
+}
+
+
+function ProjectItem({id,name,about,technologies}) {
   return (
-    <div className="project-item">
+    <div className="project-item" key={id}>
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        <RenderSpan technologies={technologies}/>
       </div>
     </div>
   );
